68a1772940c3b348677de1c500c4bbaf37787546
Lucas dos Santos Antunes <lucas.antunes.santos81@gmail.com>
2024-08-15 21:13:02 -0400

    Buscar Documentos

--- ViewModel/SearchViewModel.cs
+++ ViewModel/SearchViewModel.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using EasyPaperWork.Models;
+using EasyPaperWork.Services;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -12,6 +16,9 @@ namespace EasyPaperWork.ViewModel
     public class SearchViewModel:INotifyPropertyChanged
     {
         private string EntryArchiveName;
+        private Documents Documento;
+        public ObservableCollection<Documents> DocumentCollection { get; set; }
+        public FirebaseService firebaseService;
         public ICommand BtSearch { get; }
         public string _EntryArchiveName {
             get{
@@ -21,15 +28,20 @@ namespace EasyPaperWork.ViewModel
                 OnPropertyChanged(nameof(EntryArchiveName));
             } 
         }
+       
 
         
         public SearchViewModel() {
             BtSearch = new Command(async () => await SearchArchive());
-        
+            DocumentCollection = new ObservableCollection<Documents>();
+            Documento = new Documents();
+            firebaseService = new FirebaseService();
         }
         private async Task SearchArchive()
         {
-
+            if (EntryArchiveName != null)
+               Documento = await firebaseService.BuscarDocumentosNaMainPageFilesAsync("Users", AppData.UserUid, "Documents", EntryArchiveName);
+            DocumentCollection.Add(Documento);
         }
         public event PropertyChangedEventHandler PropertyChanged;
 
