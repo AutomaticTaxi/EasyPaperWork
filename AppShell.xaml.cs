08d136c54beb4090fdce317f17360ab436fa8481
Lucas dos Santos Antunes <lucas.antunes.santos81@gmail.com>
2024-11-04 06:15:03 -0400

    Removendo telas não utilizadas

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -9,7 +9,6 @@ public partial class AppShell : Shell
 		InitializeComponent();
         Routing.RegisterRoute("Page_Login", typeof(LoginPage));
         Routing.RegisterRoute("Page_Register_User", typeof(Page_Register_User));
-        Routing.RegisterRoute("Page_Document_Upload", typeof(Page_Document_Upload));
         Routing.RegisterRoute("Main_Page_Files", typeof(Main_Page_Files));
 
 
